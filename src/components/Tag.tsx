927cdb1cf51e89d95ee0fbad386952c9f58c612a
netgamesekai <121766552+netgamesekai@users.noreply.github.com>
2024-04-28 23:50:50 +0900

    fix: interests overflow on mobile platform

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -7,7 +7,7 @@ interface TagProps {
 
 export const Tag: React.FC<TagProps> = ({ name, className = "" }) => (
   <div
-    className={`text-neutral-300 font-normal flex flex-row outline outline-0 rounded text-xl bg-[#2C2F33] space-x-0.5 p-1${
+    className={`text-neutral-300 font-normal flex flex-row outline outline-0 rounded text-xl bg-[#2C2F33] mr-3 mb-3 space-x-0.5 p-1${
       className ? " " + className : ""
     }`}
   >
